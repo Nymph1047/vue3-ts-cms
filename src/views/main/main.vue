92f1bd9b6f60ed4ab9f0cd57c5080ea8b2adfb97
xuyl <xyl13129752627@163.com>
2022-08-30 15:58:44 +0800

    新增vuex跟vue-router配置，以及perttier格式化代码

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="app">
-    <h2>{{ $store.state.name }}</h2>
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">首页</router-link>
-    <router-view></router-view>
-  </div>
+  <div>main</div>
 </template>
 
 <script lang="ts">
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 1、配置方式一：CLI提供的属性
+  outputDir: "./build",
+  // 2、配置方式二：和webpack属性完全一致，最后会进行合并
+  configureWebpack: {
+    resolve: {
+      alias: {
+        comments: "@/component",
+      },
+    },
+  },
 });
