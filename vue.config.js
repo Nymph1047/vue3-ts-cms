7e29a0ac709886647ec22b792695eee7b9946076
xuyl <xyl13129752627@163.com>
2022-08-30 15:59:50 +0800

    perttier格式化代码

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 1、配置方式一：CLI提供的属性
-  outputDir: "./build",
+  outputDir: './build',
   // 2、配置方式二：和webpack属性完全一致，最后会进行合并
   configureWebpack: {
     resolve: {
       alias: {
-        comments: "@/component",
-      },
-    },
-  },
-});
+        comments: '@/component'
+      }
+    }
+  }
+})
