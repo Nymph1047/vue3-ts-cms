77c63fea41e8bca1b8a785c2112fafe88a0b3e44
xuyl <xyl13129752627@163.com>
2022-08-31 17:40:42 +0800

    全局配置elementplus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
     <h2>{{ $store.state.name }}</h2>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
+
+// 全局引入
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// import 'element-plus/theme-chalk/base.css'
+// import { ElButton } from 'element-plus/lib/index'
+
 import App from './App.vue'
 
 import router from '@/router'
 import store from '@/store'
 
 const app = createApp(App)
+
 app.use(router)
 app.use(store)
+app.use(ElementPlus)
 app.mount('#app')
